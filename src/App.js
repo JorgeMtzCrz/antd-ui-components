c86a155ccc9b0222e5a0efe1d37ac671ace59161
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-08-04 10:22:13 -0500

    push all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import LayoutC from './components/Layout'
+import PaginationC from './components/PaginationC'
+import { Rate, Popover, Button } from 'antd'
+
+const content = <p>¿Cómo estuvo antd?</p>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LayoutC>
+      <PaginationC />
+      <Popover content={content} title="Jalooooo">
+        <Button type="primary">Hover me</Button>
+      </Popover>
+      <Rate allowHalf defaultValue={2.5} />
+    </LayoutC>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/PaginationC.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Pagination } from 'antd'
+
+const PaginationC = () => {
+  return <Pagination defaultCurrent={1} total={50} />
+}
+
+export default PaginationC
